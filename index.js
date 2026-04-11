1ef91ac4a581880e4bfea9b7a3534375789e6605
Artem Zhyrnyi <ic3ego@gmail.com>
2026-04-11 08:42:25 +0300

    minor code refactoring

--- index.js
+++ index.js
@@ -27,17 +27,13 @@ let persons = [
   },
 ];
 
+app.use(cors());
 app.use(express.static("dist"));
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
 app.use(express.json());
+
 morgan.token("body", (req) => {
   return req.method === "POST" ? JSON.stringify(req.body) : " ";
 });
-
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
 );
