59b95d637780d578755356d66b64071f93922db2
Artem Zhyrnyi <ic3ego@gmail.com>
2026-04-12 10:27:19 +0300

    switch from /api/persons to /api/notes

--- index.js
+++ index.js
@@ -1,106 +1,79 @@
 const express = require("express");
-const cors = require("cors");
 const app = express();
 
-let persons = [
+let notes = [
   {
     id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
+    content: "HTML is easy",
+    important: true,
   },
   {
     id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
+    content: "Browser can execute only JavaScript",
+    important: false,
   },
   {
     id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
   },
 ];
 
-const logger = (req, _, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-app.use(logger);
-app.use(
-  cors({
-    origin: "https://v-test.pages.dev",
-  }),
-);
 app.use(express.json());
 
-app.get("/api/persons", (_, res) => {
-  res.json(persons);
+app.get("/", (_, response) => {
+  response.send("<h1>Hello World!</h1>");
 });
 
-app.get("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  const person = persons.find((p) => p.id === id);
-
-  if (person) {
-    return res.json(person);
-  }
-
-  res.status(404).json({ error: "Person not found" });
+app.get("/api/notes", (_, response) => {
+  response.json(notes);
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
+app.get("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  const note = notes.find((note) => note.id === id);
 
-  const personToDelete = persons.find((p) => p.id === id);
-  if (!personToDelete) {
-    return res.status(404).json({ error: "Person not found" });
+  if (note) {
+    response.json(note);
+  } else {
+    response.status(404).end();
   }
-
-  persons = persons.filter((p) => p.id !== id);
-  res.json(personToDelete);
 });
 
 const generateId = () => {
-  return String(Math.trunc(Math.random() * 1_000_000 + 1));
+  const maxId =
+    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
+  return String(maxId + 1);
 };
 
-app.post("/api/persons", (req, res) => {
-  const person = req.body;
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
 
-  if (!person.name || !person.number) {
-    return res
-      .status(400)
-      .json({ error: "Invalid JSON. Missing name or number" });
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
   }
 
-  const hasName = persons.some(
-    (p) => p.name.toLowerCase().trim() === person.name.toLowerCase().trim(),
-  );
-
-  if (hasName) {
-    return res.status(400).json({ error: "Name must be unique" });
-  }
-
-  const newPerson = {
-    ...person,
+  const note = {
+    content: body.content,
+    important: body.important || false,
     id: generateId(),
   };
 
-  persons = [...persons, newPerson];
+  notes = notes.concat(note);
 
-  res.status(201).json(newPerson);
+  response.json(note);
 });
 
-const unknownEndpoint = (_, res) => {
-  res.status(404).json({ error: "Unknown endpoint" });
-};
-app.use(unknownEndpoint);
+app.delete("/api/notes/:id", (request, response) => {
+  const id = request.params.id;
+  notes = notes.filter((note) => note.id !== id);
+
+  response.status(204).end();
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
