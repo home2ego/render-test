7e8388f52ddaff3ad0c68001e1fa4aca19cd7e82
Artem Zhyrnyi <ic3ego@gmail.com>
2026-04-14 10:05:43 +0300

    create new note and add to MongoDB

--- index.js
+++ index.js
@@ -21,35 +21,24 @@ app.get("/api/notes/:id", (request, response) => {
         return response.json(note);
       }
 
-      response.status(404).send("Note not found");
+      response.status(404).send("note not found");
     })
-    .catch(() => response.status(404).send("Note not found"));
+    .catch(() => response.status(404).send("note not found"));
 });
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
 
 app.post("/api/notes", (request, response) => {
   const body = request.body;
 
   if (!body.content) {
-    return response.status(400).json({
-      error: "content missing",
-    });
+    return response.status(400).send("content missing");
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
-    id: generateId(),
-  };
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => response.json(savedNote));
 });
 
 app.delete("/api/notes/:id", (request, response) => {
