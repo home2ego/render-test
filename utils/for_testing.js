bc056d06c33c6e4e0985284792119789623c9d63
Artem Zhyrnyi <ic3ego@gmail.com>
2026-04-26 21:29:24 +0300

    add initial notes

--- utils/for_testing.js
+++
@@ -1,9 +0,0 @@
-const reverse = (string) => string.split('').reverse().join('');
-
-const average = (array) => {
-  const sum = array.reduce((acc, item) => acc + item, 0);
-
-  return array.length === 0 ? 0 : sum / array.length;
-};
-
-module.exports = { reverse, average };
